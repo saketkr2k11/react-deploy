efa4aa973fe02ebd9abd87873809e825b073f7bf
unknown <saketkr2k11@gmail.com>
2020-08-14 14:24:35 +0530

    Updates

---
+++ precache-manifest.46cba2cd1e768bdd8945fb40f6cb65fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123c8c1760f962d59b83c9a2bc91bcfd",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "735446ac123a12120c10",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/react-deploy/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735446ac123a12120c10",
+    "url": "/react-deploy/static/js/main.6d25f9e7.chunk.js"
+  },
+  {
+    "revision": "86d98ee0cb1baf327b93",
+    "url": "/react-deploy/static/js/runtime-main.a7f1a508.js"
+  }
+]);
